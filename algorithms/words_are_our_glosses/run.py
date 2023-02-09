60db9cabd23f0fe4d4379a97c10458ba00bbe580
Alix <alix.declerck@student.umons.ac.be>
2023-02-09 17:29:27 +0100

    update word are our glosses : generalize metrics and decoder to common package

--- algorithms/words_are_our_glosses/run.py
+++ algorithms/words_are_our_glosses/run.py
@@ -57,48 +57,52 @@ from NMT_utils import read_corpus, batch_iter
 from nmt_model import Hypothesis, NMT
 from vocab import Vocab
 
-
-def evaluate_ppl(model, dev_data, batch_size=32):
-    """ Evaluate perplexity on dev sentences
-    @param model (NMT): NMT Model
-    @param dev_data (list of (src_sent, tgt_sent)): list of tuples containing source and target sentence
-    @param batch_size (batch size)
-    @returns ppl (perplixty on dev sentences)
-    """
-    was_training = model.training
-    model.eval()
-
-    cum_loss = 0.
-    cum_tgt_words = 0.
-
-    # no_grad() signals backend to throw away all gradients
-    with torch.no_grad():
-        for src_sents, tgt_sents in batch_iter(dev_data, batch_size):
-            loss = -model(src_sents, tgt_sents).sum()
-
-            cum_loss += loss.item()
-            tgt_word_num_to_predict = sum(len(s[1:]) for s in tgt_sents)  # omitting leading `<s>`
-            cum_tgt_words += tgt_word_num_to_predict
-
-        ppl = np.exp(cum_loss / cum_tgt_words)
-
-    if was_training:
-        model.train()
-
-    return ppl
+from common.output_decoder import beam_search
+from common.metrics.bleu import compute_corpus_level_bleu_score
+from common.metrics.perplexity import evaluate_ppl
 
 
-def compute_corpus_level_bleu_score(references: List[List[str]], hypotheses: List[Hypothesis]):  # -> float:
-    """ Given decoding results and reference sentences, compute corpus-level BLEU score.
-    @param references (List[List[str]]): a list of gold-standard reference target sentences
-    @param hypotheses (List[Hypothesis]): a list of hypotheses, one for each reference
-    @returns bleu_score: corpus-level BLEU score
-    """
-    if references[0][0] == '<s>':
-        references = [ref[1:-1] for ref in references]
-    bleu_score = corpus_bleu([[ref] for ref in references],
-                             [hyp.value for hyp in hypotheses])
-    return bleu_score
+# def evaluate_ppl(model, dev_data, batch_size=32):
+#     """ Evaluate perplexity on dev sentences
+#     @param model (NMT): NMT Model
+#     @param dev_data (list of (src_sent, tgt_sent)): list of tuples containing source and target sentence
+#     @param batch_size (batch size)
+#     @returns ppl (perplixty on dev sentences)
+#     """
+#     was_training = model.training
+#     model.eval()
+#
+#     cum_loss = 0.
+#     cum_tgt_words = 0.
+#
+#     # no_grad() signals backend to throw away all gradients
+#     with torch.no_grad():
+#         for src_sents, tgt_sents in batch_iter(dev_data, batch_size):
+#             loss = -model(src_sents, tgt_sents).sum()
+#
+#             cum_loss += loss.item()
+#             tgt_word_num_to_predict = sum(len(s[1:]) for s in tgt_sents)  # omitting leading `<s>`
+#             cum_tgt_words += tgt_word_num_to_predict
+#
+#         ppl = np.exp(cum_loss / cum_tgt_words)
+#
+#     if was_training:
+#         model.train()
+#
+#     return ppl
+
+
+# def compute_corpus_level_bleu_score(references: List[List[str]], hypotheses: List[Hypothesis]):  # -> float:
+#     """ Given decoding results and reference sentences, compute corpus-level BLEU score.
+#     @param references (List[List[str]]): a list of gold-standard reference target sentences
+#     @param hypotheses (List[Hypothesis]): a list of hypotheses, one for each reference
+#     @returns bleu_score: corpus-level BLEU score
+#     """
+#     if references[0][0] == '<s>':
+#         references = [ref[1:-1] for ref in references]
+#     bleu_score = corpus_bleu([[ref] for ref in references],
+#                              [hyp.value for hyp in hypotheses])
+#     return bleu_score
 
 
 def train(args: Dict):
@@ -297,28 +301,28 @@ def decode(args: Dict[str, str]):
             f.write(hyp_sent + '\n')
 
 
-def beam_search(model: NMT, test_data_src: List[List[str]], beam_size: int, max_decoding_time_step: int): # -> List[List[Hypothesis]]:
-    """ Run beam search to construct hypotheses for a list of src-language sentences.
-    @param model (NMT): NMT Model
-    @param test_data_src (List[List[str]]): List of sentences (words) in source language, from test set.
-    @param beam_size (int): beam_size (# of hypotheses to hold for a translation at every step)
-    @param max_decoding_time_step (int): maximum sentence length that Beam search can produce
-    @returns hypotheses (List[List[Hypothesis]]): List of Hypothesis translations for every source sentence.
-    """
-    was_training = model.training
-    model.eval()
-
-    hypotheses = []
-    with torch.no_grad():
-        for src_sent in tqdm(test_data_src, desc='Decoding', file=sys.stdout):
-            example_hyps = model.beam_search(src_sent, beam_size=beam_size, max_decoding_time_step=max_decoding_time_step)
-
-            hypotheses.append(example_hyps)
-
-    if was_training:
-        model.train(was_training)
-
-    return hypotheses
+# def beam_search(model: NMT, test_data_src: List[List[str]], beam_size: int, max_decoding_time_step: int): # -> List[List[Hypothesis]]:
+#     """ Run beam search to construct hypotheses for a list of src-language sentences.
+#     @param model (NMT): NMT Model
+#     @param test_data_src (List[List[str]]): List of sentences (words) in source language, from test set.
+#     @param beam_size (int): beam_size (# of hypotheses to hold for a translation at every step)
+#     @param max_decoding_time_step (int): maximum sentence length that Beam search can produce
+#     @returns hypotheses (List[List[Hypothesis]]): List of Hypothesis translations for every source sentence.
+#     """
+#     was_training = model.training
+#     model.eval()
+#
+#     hypotheses = []
+#     with torch.no_grad():
+#         for src_sent in tqdm(test_data_src, desc='Decoding', file=sys.stdout):
+#             example_hyps = model.beam_search(src_sent, beam_size=beam_size, max_decoding_time_step=max_decoding_time_step)
+#
+#             hypotheses.append(example_hyps)
+#
+#     if was_training:
+#         model.train(was_training)
+#
+#     return hypotheses
 
 
 def main():

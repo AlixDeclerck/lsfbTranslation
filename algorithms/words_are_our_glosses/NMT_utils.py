b456e2d2ba23cacc300b4a8f30dd4c87c5e4a596
Alix <alix.declerck@student.umons.ac.be>
2023-02-18 22:15:58 +0100

    update architecture : move beam search to common package

--- algorithms/words_are_our_glosses/NMT_utils.py
+++ algorithms/words_are_our_glosses/NMT_utils.py
@@ -10,13 +10,10 @@ Vera Lin <veralin@stanford.edu>
 """
 
 import math
-from typing import List
 
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import nltk
+import numpy as np
+
 nltk.download('punkt')
 
 
